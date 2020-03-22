55acbde85140be131842c8b19c6ca4fa965041c6
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2020-03-22 09:05:19 -0600

    wip

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import path from 'path'
+
+const app = express()
+
+// Middleware
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback')
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), () => {
+  console.log('App listening on port: '+ app.get('port'))
+});
