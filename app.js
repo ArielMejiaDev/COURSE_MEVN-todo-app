61c0a734f6f6e373fe6286402eaa5b86f1a95b8f
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2020-03-23 01:34:36 -0600

    wip

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import path from 'path'
+const mongoose = require('mongoose');
 
 const app = express()
+
+// DB connection
+const uri = 'mongodb://localhost:27017/mevn';
+const options = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true};
+
+mongoose.connect(uri, options).then(
+  () => { console.log('connect to mongodb') },
+  err => { err }
+);
 
 // Middleware
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/api', require('./routes/note'))
 
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback')
---
+++ routes/note.js
@@ -0,0 +1,81 @@
+import express, { request, response } from 'express'
+const router = express.Router()
+
+// Import note model
+import Note from '../models/note'
+
+// Add a note
+router.post('/notes', async(request, response) => {
+  const body = request.body;  
+  try {
+    const noteDB = await Note.create(body)
+    response.status(200).json(noteDB)
+  } catch (error) {
+    return response.status(500).json({
+      message: 'Whoops there is an error',
+      error: error
+    })
+  }
+})
+
+// fetch a single note
+router.get('/notes/:id', async(request, response) => {
+  const _id = request.params.id
+  try {
+    const noteDB = await Note.findOne({_id})
+    response.json(noteDB)
+  } catch (error) {
+    returnError(response, error, 400, "Whoops there is an error")
+  }
+})
+
+// all notes
+router.get('/notes', async(request, response) => {
+  try {
+    const noteDB = await Note.find()
+    response.json(noteDB)
+  } catch (error) {
+    returnError(response, error, 400, "Whoops there is an error")
+  }
+})
+
+// delete a note 
+router.delete('/notes/:id', async(request, response) => {
+  const _id = request.params.id //the const cannot be id it must be _id as the value in mongo db
+  try {
+    const noteDB = await Note.findByIdAndDelete(_id)
+    response.json(noteDB)
+  } catch (error) {
+    returnError(response, error, 400, "Error note cannot be deleted")
+  }
+})
+
+// update a note 
+router.put('/notes/:id', async(request, response) => {
+  const _id = request.params.id
+  const body = request.body
+  try {
+    const noteDB = await Note.findByIdAndUpdate(_id, body, {new: true})
+    response.json(noteDB)
+  } catch (error) {
+    returnError(response, error, 404, "Note not found")
+  }
+})
+
+function returnError(response, error, statusCode, message) {
+  return response.status(statusCode).json({
+    message: message,
+    error: error
+  })
+}
+
+// Export express app config
+module.exports = router;
+
+// notes
+
+// how to handle a different value than _id ?
+// how to validate that fields does not repeat
+// how to validate with logic not db that a certain field match the type required 
+// how to validate another note does not repeat the same info 
+// what is the correct way to add helpers to DRY
